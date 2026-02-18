1df43ab8b3065ce8c267137ce5c4d28d616d5526
iamnidjat <108131709+iamnidjat@users.noreply.github.com>
2026-02-18 18:21:05 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI
-
+from app.api.routers.transliteration import router as transliteration
 
 app = FastAPI(
     title="MyTransliterator",
     version="0.1.0",
 )
 
-# app.include_router()
+app.include_router(transliteration)
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}

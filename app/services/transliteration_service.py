059598af6cf3d2535cf6c6846f318f40934b9759
Nijat <gurbanli.nidjat001@gmail.com>
2026-02-20 22:47:11 +0400

    update transliteration service logics, stop tracking dev_seed.py

--- app/services/transliteration_service.py
+++ app/services/transliteration_service.py
@@ -1,23 +1,26 @@
 from sqlalchemy.orm import Session
 from app.api.schemas.transliteration import SuccessfulTransliteration
-from app.constants.transliteration import az_cyrillic_to_latin, az_latin_to_cyrillic
+from app.constants.transliteration import az_cyrillic_to_latin_lower, az_cyrillic_to_latin_upper, \
+    az_latin_to_cyrillic_lower, az_latin_to_cyrillic_upper
 from app.core.models.transliteration_model import Transliteration
 
 
 def from_cyrillic_to_latin_az(cyrillic_text: str, db: Session):
-    return _transliterate(cyrillic_text, az_cyrillic_to_latin, db)
+    return _transliterate(cyrillic_text, az_cyrillic_to_latin_lower, az_cyrillic_to_latin_upper, db)
 
 def from_latin_to_cyrillic_az(cyrillic_text: str, db: Session):
-    return _transliterate(cyrillic_text, az_latin_to_cyrillic, db)
+    return _transliterate(cyrillic_text, az_latin_to_cyrillic_lower, az_latin_to_cyrillic_upper, db)
 
-def _transliterate(text: str, mapping: dict[str, str], db: Session):
+def _transliterate(text: str, mapping_lower: dict[str, str], mapping_upper: dict[str, str], db: Session):
     result = []
     unrecognized = []
 
     for ch in text:
         if ch.isalpha():
-            if ch in mapping:
-                result.append(mapping.get(ch))
+            if ch in mapping_lower:
+                result.append(mapping_lower.get(ch))
+            elif ch in mapping_upper:
+                result.append(mapping_upper.get(ch))
             else:
                 result.append('?') # adding ? to specify that the symbol is unrecognized
                 unrecognized.append(ch)
